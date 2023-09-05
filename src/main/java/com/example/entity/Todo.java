61aab910d212190868d9520ba7e52a3cf6756fd6
beqirajd <dejvis.beqiraj@linksmt.it>
2023-09-05 18:00:33 +0200

    add entity validations

--- src/main/java/com/example/entity/Todo.java
+++ src/main/java/com/example/entity/Todo.java
@@ -2,7 +2,11 @@ package com.example.entity;
 
 import lombok.Data;
 
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -13,8 +17,14 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty(message = "Task must be set")
     private String task;
+
+    @NotNull(message = "Due date must be set")
+    @FutureOrPresent(message = "Due date must be in the present or the future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     private boolean isCompleted;
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
