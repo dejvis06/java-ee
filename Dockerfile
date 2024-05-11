67ae50a8e5ad4bd11f58e0fb74a51c322792a057
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-11 18:53:12 +0200

    fix build

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM airhacks/glassfish
-COPY ./target/java-ee.war ${DEPLOYMENT_DIR}
+FROM glassfish:5.0
+COPY ./target/java-ee-0.0.1-SNAPSHOT.war ${DEPLOYMENT_DIR}
--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t com.example/java-ee .
-docker rm -f java-ee || true && docker run -d -p 8080:8080 -p 4848:4848 --name java-ee com.example/java-ee 
+mvn clean package && docker build -t java-ee .
+docker rm -f java-ee || true && docker run -p 8080:8080 -p 4848:4848 --name java-ee com.example/java-ee
