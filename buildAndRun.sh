16172748e03106a14efdf4917085789fa5508435
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-12 12:22:40 +0200

    rm -f container if exists

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean install && docker build -t com.example/java-ee .
-docker run -p 8080:8080 -p 4848:4848 --name java-ee com.example/java-ee
+docker rm -f java-ee || true && docker run -p 8080:8080 -p 4848:4848 --name java-ee com.example/java-ee
